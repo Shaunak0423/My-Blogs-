ad3cc0b137b28295c2037c0caf36572fde462e92
Shaunak0423 <shetshaunak23@gmail.com>
2025-10-13 12:31:25 +0530

    backend changes for image update and delete

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY", default="unsafe-secret")
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = True
 
 
 # Quick-start development settings - unsuitable for production
--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -1,12 +1,11 @@
-# blog/serializers.py
 from rest_framework import serializers
 from .models import Posts
 
 class PostSerializer(serializers.ModelSerializer):
     author_username = serializers.ReadOnlyField(source="author.username")
+    image = serializers.ImageField(use_url=True)
 
     class Meta:
         model = Posts
-        fields = ["id", "title", "content", "author", "author_username","image", "created_at"]
+        fields = ["id", "title", "content", "author", "author_username", "image", "created_at"]
         read_only_fields = ["author"]
-
